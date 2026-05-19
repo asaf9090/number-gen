683337fd909d93ba8622770d8f730fb90bb472f5
asaf9090 <mdasad732475@gmail.com>
2026-05-20 02:42:07 +0600

    feat: completed setup with number generator name

---
+++ lotto-wheeler/public/service-worker.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = "lotto-wheeler-v1";
+const ASSETS = ["/", "/index.html", "/manifest.json"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      return res || fetch(e.request);
+    })
+  );
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      );
+    })
+  );
+});
